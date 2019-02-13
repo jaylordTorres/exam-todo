7053f9eb7a551b0a32a5cdf6407b1f761302d883
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-02-13 21:39:59 +0800

    setup route structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './container/App'
+import createStore from './store/createStore'
+import Route from './route/'
+
+const store = createStore()
+const routes = Route(store)
+
+ReactDOM.render(
+	<App store={store} routes={routes} />,
+	document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
---
+++ src/route/NotFound/components/NotFoundPage/NotFoundPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const NotFoundPage = () => (
+  <div>
+	  <center>
+	    <h1>Whoops! 404!</h1>
+	    <p>This page was not found.</p>
+	  </center>
+  </div>
+)
+
+NotFoundPage.propTypes = {
+}
+
+export default NotFoundPage
